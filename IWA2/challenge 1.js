00e20d02502bcfbc801a70d47abe719b4d015a4b
KeenanJohnson28 <126468536+KeenanJohnson28@users.noreply.github.com>
2023-03-28 13:25:34 +0200

    Add files via upload

---
+++ IWA2/challenge 1.js
@@ -0,0 +1,12 @@
+/* This is the firstname of the user */
+const user ='Keenan'
+
+/* This is the lastname of the user */
+const surname ='Johnson'
+
+/**
+ * this comment is meant to exhibit follow up comments
+ * the next function shows the function
+ */
+
+console.log( user , surname )
