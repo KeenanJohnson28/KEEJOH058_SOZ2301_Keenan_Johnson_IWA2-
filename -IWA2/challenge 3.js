2b4cb6a3d6ebbfa426662f8f377e94f4321fd71d
KeenanJohnson28 <126468536+KeenanJohnson28@users.noreply.github.com>
2023-03-29 11:37:47 +0200

    Add files via upload

---
+++ -IWA2/challenge 3.js
@@ -0,0 +1,16 @@
+/**It is important to show the following message 
+ * in order to:
+ * to assure users,
+ * to scare hackers,
+ * to impress investors
+ * 
+ */
+
+//in the next line , quoatation marks were missing, the statement would not show without it
+console.warn('Security scan starting')
+
+//It is important to let user know when they can close the page  
+
+//in the next line , quoatation marks were missing at the end before the closing bracket, the statement would not show without it
+
+console.info('Please wait for scan to complete before closing the browser.')
